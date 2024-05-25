592bb898eaf0233edc6c3c4b6df4877c29385464
alvarosolan083 <135790504+alvarosolan083@users.noreply.github.com>
2024-05-25 14:31:09 -0400

    Actualizar db.php

--- db.php
+++ db.php
@@ -8,7 +8,7 @@ try {
     $pdo = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
-    die("Could not connect to the database $db :" . $e->getMessage());
+    die(" No se pudo conectar a la base de datos$db :" . $e->getMessage());
 }
 ?>
 
